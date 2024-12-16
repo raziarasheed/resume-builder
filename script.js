ab2fd514802d34bfdf1c83d58b81d0405df73dd3
raziarasheed <154565353+raziarasheed@users.noreply.github.com>
2024-12-17 03:41:11 +0500

    resume

---
+++ script.js
@@ -0,0 +1,13 @@
+var toggleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
+// toggleButton.addEventListener('click', () => {
+//     skills.classList.toggle('hidden');}
+// );
